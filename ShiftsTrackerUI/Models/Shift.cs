4a8846f0eedb8ac4884f66deecd7d1891a3a7cf6
Walter4B <walter.bali.markovi@gmail.com>
2022-05-06 18:23:47 +0200

    Setup project in the same repo as API

---
+++ ShiftsTrackerUI/Models/Shift.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftsTrackerUI.Models
+{
+    internal class Shift
+    {
+        public int ShiftId { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        public decimal Pay { get; set; }
+
+        public decimal Minutes { get; set; }
+
+        public string Location { get; set; }
+    }
+}
--- ShiftsTrackerUI/Program.cs
+++ ShiftsTrackerUI/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ShiftTrackerUI
+{ 
+    class Program
+    {
+
+        public void Main()
+        { 
+        
+        }
+    }
+}
+    
